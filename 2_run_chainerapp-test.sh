32893381dbbce987f39b0e83b85f71924a9add4d
Naoki Ueda <naoki.dx.xb@gmail.com>
2015-07-12 11:20:22 +0900

    Dockerfile修正 1.chainerをインストールまで組み込む 2.chainerデータ格納ディレクトリをマウント

--- 2_run_chainerapp-test.sh
+++ 2_run_chainerapp-test.sh
@@ -4,5 +4,5 @@
 #
 docker run --name chainerapp-test-up -i -t -d \
   -v $GIT_HOME/chainerapp:/opt/t4j/chainerapp \
-  -v $GIT_HOME/chainer:/opt/t4j/chainer \
+  -v $GIT_HOME/chainer-data:/var/opt/t4j/chainer \
   t4j/chainerapp-test:latest
